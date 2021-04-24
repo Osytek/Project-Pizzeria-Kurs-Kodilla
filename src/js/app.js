4e4381ce388956d1a5830f793f68550f92a9fc17
Radosław Osytek <Osytek96@gmail.com>
2021-04-24 11:26:44 +0200

    Finish 10th module

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,71 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+    initBooking: function(){
+      const thisApp = this;
+      const widgetElem = document.querySelector(select.containerOf.booking);
+      thisApp.booking = new Booking(widgetElem);
+
+      
+    },
+
+    initPages: function(){
+      const thisApp = this;
+
+      thisApp.pages = document.querySelector(select.containerOf.pages).children;
+
+      thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+      const idFromHash = window.location.hash.replace('#/', '');
+      
+
+      let pageMatchingHash = thisApp.pages[0].id;
+
+      for(let page of thisApp.pages){
+        if(page.id == idFromHash){
+          pageMatchingHash = page.id;
+          break;
+        }
+      }
+
+      thisApp.activatePage(pageMatchingHash);
+
+      for(let link of thisApp.navLinks){
+        link.addEventListener('click', function(event){
+          const clikedElement = this;
+          event.preventDefault();
+
+          /* get page id from href attribute */
+          const id = clikedElement.getAttribute('href').replace('#', '');
+
+          /* run this.App.activatePage with that id */
+          thisApp.activatePage(id);
+
+          /* change URL hash */
+          window.location.hash = '#/' + id;
+        });
+      }
+    },
+
+    activatePage: function(pageId){
+      const thisApp = this;
+
+      /* add class "active" to matching pages, remove from non-matching */
+      for(let page of thisApp.pages){
+        page.classList.toggle(classNames.pages.active, page.id == pageId);
+      }
+
+      /* add class "active" to matching links, remove from non-matching */
+      for(let link of thisApp.navLinks){
+        link.classList.toggle(
+          classNames.nav.active,
+          link.getAttribute('href') == '#' + pageId
+        );
+      }
+    },
     initMenu: function(){
       const thisApp = this;
       
@@ -14,6 +77,8 @@ const app = {
       const thisApp = this;
       const cartElem = document.querySelector(select.containerOf.cart);
       thisApp.cart = new Cart(cartElem);
+
+      
 
       thisApp.productList = document.querySelector(select.containerOf.menu);
 
@@ -48,6 +113,10 @@ const app = {
       thisApp.initData();
       
       thisApp.initCart();
+
+      thisApp.initPages();
+
+      thisApp.initBooking();
     },
   };
 
---
+++ src/js/components/Booking.js
@@ -0,0 +1,43 @@
+import {templates, select} from '../settings.js';
+import AmountWidget from './AmountWidgets.js';
+
+
+class Booking{
+    constructor(element){
+        const thisBooking = this;
+        
+        thisBooking.render(element);
+        thisBooking.initWidgets();
+    }
+
+    render(element){
+        const thisBooking = this;
+
+        /* generate HTML based on template */
+        
+        const generatedHTML = templates.bookingWidget(element);
+
+        thisBooking.dom = {};
+        thisBooking.dom.wrapper = element;
+        
+        console.log('asdasd', generatedHTML);
+
+        thisBooking.dom.wrapper.innerHTML = generatedHTML;
+
+        thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+        thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+
+        thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+        thisBooking.dom.peopleAmount.addEventListener('updated', function(){
+
+        });
+        thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+        thisBooking.dom.hoursAmount.addEventListener('updated', function(){
+
+        });
+    }
+    initWidgets(){
+
+    }
+}
+export default Booking;
