ea777d8c870b562b69887fa9bc9af74866340c63
Radosław Osytek <Osytek96@gmail.com>
2021-04-26 08:58:41 +0200

    Finish 1st submodule

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import {templates, select} from '../settings.js';
 import AmountWidget from './AmountWidgets.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 
 class Booking{
@@ -27,6 +29,9 @@ class Booking{
         thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
         thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
 
+        thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+        thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
+
         thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
         thisBooking.dom.peopleAmount.addEventListener('updated', function(){
 
@@ -37,7 +42,10 @@ class Booking{
         });
     }
     initWidgets(){
+        const thisBooking = this;
+        thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
 
+        thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
     }
 }
 export default Booking;
